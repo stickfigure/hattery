5000cdc942a0c0c652cba04befa682a15b65656e
Jeff Schnitzer <jeff@infohazard.org>
2018-06-28 09:55:41 -0700

    Even more logging reduction

--- src/main/java/com/voodoodyne/hattery/BinaryAttachment.java
+++ src/main/java/com/voodoodyne/hattery/BinaryAttachment.java
@@ -1,10 +1,13 @@
 package com.voodoodyne.hattery;
 
 import lombok.Data;
+import lombok.ToString;
+
 import java.io.InputStream;
 
 /** Used as a param value when user submits binary attachments */
 @Data
+@ToString(exclude = "data")
 public class BinaryAttachment {
 	private final InputStream data;
 	private final String contentType;
